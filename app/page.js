a0e8e495b219c59e462848d47dc4ab394452c236
muafalah <muh.ifa.amrillah@gmail.com>
2023-09-06 18:23:56 +0700

    feat: add navbar component

--- app/page.js
+++ app/page.js
@@ -1,3 +1,10 @@
+import "./page.css";
+import Navbar from "@/components/Navbar/Navbar";
+
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className="app">
+      <Navbar />
+    </div>
+  );
 }
---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { useState } from "react";
+import { BiMenuAltRight } from "react-icons/bi";
+import { RxCross2 } from "react-icons/rx";
+
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [mobileMenuOpened, setMobileMenuOpened] = useState(false);
+
+  return (
+    <div className="n-wrapper">
+      {/* Desktop Version */}
+      <div className="container">
+        <div className="n-container">
+          {/* Left Side */}
+          <div className="n-logo">
+            <span>DIGIFUND</span>
+          </div>
+          {/* Right Side */}
+          <div className="n-right">
+            <div className="n-menu">
+              <span>What we do</span>
+              <span>How it works</span>
+              <span>Who we invest in</span>
+              <span>Testimonials</span>
+            </div>
+            <div className="fund-button">Get Funded</div>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile/Tab Version */}
+      <div className="nm-container">
+        {/* Logo */}
+        <span>DIGIFUND</span>
+
+        {/* Menu Icon */}
+        {!mobileMenuOpened ? (
+          <BiMenuAltRight
+            size={30}
+            onClick={() => setMobileMenuOpened(true)}
+            style={{ cursor: "pointer" }}
+          />
+        ) : (
+          <RxCross2
+            size={30}
+            onClick={() => setMobileMenuOpened(false)}
+            style={{ cursor: "pointer" }}
+          />
+        )}
+
+        {/* Mobile Menu */}
+        <div
+          className="nm-menu"
+          style={{ transform: mobileMenuOpened && "translateX(0%)" }}
+        >
+          <span>What we do</span>
+          <span>How it works</span>
+          <span>Who we invest in</span>
+          <span>Testimonials</span>
+          <div className="m-funded-button">Get Funded</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
