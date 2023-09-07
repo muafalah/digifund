0983d2661ac10b3c1b77828a765ae37f2d8c1595
muafalah <muh.ifa.amrillah@gmail.com>
2023-09-07 10:16:38 +0700

    feat: add branding video component

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import "./page.css";
 import Navbar from "@/components/Navbar/Navbar";
 import Hero from "@/components/Hero/Hero";
+import BrandingVideo from "@/components/BrandingVideo/BrandingVideo";
 
 export default function Home() {
   return (
     <div className="app">
       <Navbar />
       <Hero />
+      <BrandingVideo />
     </div>
   );
 }
